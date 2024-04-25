e7ef02ea8f950410bb0d104bb0e94d959aee6b5e
Momoyaan <ianmathewaviso@gmail.com>
2024-04-25 20:23:29 +0800

    Add Login, Register etc. for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Suspense, lazy, useEffect, useState } from "react";
+import { Route, Routes, Navigate, BrowserRouter } from "react-router-dom";
+const Loader = lazy(() => import("./components/Loader"));
+const Error = lazy(() => import("./pages/Error"));
+const Login = lazy(() => import("./pages/auth/Login"));
+const Register = lazy(() => import("./pages/auth/Register"));
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState<boolean>(true);
 
-  return (
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  return loading ? (
+    <Loader />
+  ) : (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Suspense fallback={<Loader></Loader>}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Navigate to="/auth/login" />} />
+            <Route path="/auth/login" element={<Login />} />
+            <Route path="/auth/register" element={<Register />} />
+            <Route
+              element={<Suspense fallback={<Loader></Loader>}></Suspense>}
+            ></Route>
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
