79c690f75337367f45b78d310f01a39d7923decb
Momoyaan <ianmathewaviso@gmail.com>
2024-04-28 22:28:28 +0800

    Update error page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,25 +1,29 @@
-import { Link } from "react-router-dom";
+import "../css/Error.css";
 
 const Error = () => {
   return (
-    <div className="grid h-screen px-4 bg-white place-content-center">
-      <div className="text-center">
-        <h1 className="font-black text-gray-200 text-9xl">404</h1>
+    <main className="login-main-bg">
+      <div className="login-container">
+        <div className="login-img">
+          <img
+            className="img-login"
+            src="../public/assets/loginimg.png"
+            alt="left img"
+          />
+        </div>
+        <div className="login-form">
+          <div className="logo-title">
+            <img src="../public/assets/logo.png" alt="logo" />
+            <span className="logo-title-campus">Campus</span>&nbsp;
+            <span className="logo-title-eats">Eats</span>
+          </div>
 
-        <p className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-          Uh-oh!
-        </p>
+          <h1>Oops!</h1>
 
-        <p className="mt-4 text-gray-500">We can't find that page.</p>
-
-        <Link
-          to="/"
-          className="inline-block px-5 py-3 mt-6 text-sm font-medium text-white bg-indigo-600 rounded hover:bg-indigo-700 focus:outline-none focus:ring"
-        >
-          Go Back Home
-        </Link>
+          <span className="span-subtitle">Panugo na ari!</span>
+        </div>
       </div>
-    </div>
+    </main>
   );
 };
 
