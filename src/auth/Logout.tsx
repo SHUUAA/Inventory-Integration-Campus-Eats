c05e04db9105fb6568bc50b14a55111b8419dd3b
Momoyaan <ianmathewaviso@gmail.com>
2024-05-01 11:20:23 +0800

    Clear localstorage if user is logged out

--- src/auth/Logout.tsx
+++ src/auth/Logout.tsx
@@ -2,5 +2,6 @@ import { signOut } from "firebase/auth";
 import { authentication } from "../config/firebase";
 
 export const logout = async () => {
+    localStorage.clear();
     await signOut(authentication);
   };
