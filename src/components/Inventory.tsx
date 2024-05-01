1c9403c7173d6326052e23f8859ee0758c1b326a
Brbrynt <drdbryntdador@gmail.com>
2024-05-01 17:53:59 +0800

    Inventory Search Bar(Incomplete)

--- src/components/Inventory.tsx
+++ src/components/Inventory.tsx
@@ -1,11 +1,34 @@
-import React from "react";
-import "../css/Inventory.css";
+import React, { useState } from 'react';
+import '../css/Inventory.css';
+import searchIcon from '../../public/assets/searchicon.png';
 
-const Inventory = () => {
+const Inventory: React.FC = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setSearchTerm(event.target.value);
+  };
+
+  const handleSearchSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      console.log('Searching for:', searchTerm); 
+  };
+
   return (
-    <div>
-      <h2>Inventory</h2>
-    </div>
+      <div className="inventory-container">
+          <form onSubmit={handleSearchSubmit} className="search-form">
+              <div className="search-box">
+                  <img src={searchIcon} alt="Search" className="search-icon" onClick={handleSearchSubmit}/>
+                  <input
+                      type="text"
+                      placeholder="Search product, supplier, order"
+                      value={searchTerm}
+                      onChange={handleSearchChange}
+                      className="search-input"
+                  />
+              </div>
+          </form>
+      </div>
   );
 };
 
