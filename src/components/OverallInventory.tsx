9c138e557b83ac41f9d22334f7aa0282a19370c1
Brbrynt <drdbryntdador@gmail.com>
2024-05-03 00:32:40 +0800

    Overall Inventory
    Add Info

--- src/components/OverallInventory.tsx
+++ src/components/OverallInventory.tsx
@@ -3,18 +3,31 @@ import '../css/OverallInventory.css';
 const OverallInventory = () => {
   return (
     <div>
-      <div className="grid grid-cols-1 gap-4 lg:gap-8">
-        <div className="inv-inventory-container">
-          <h1 className="inv-inventory-header">Overall Inventory</h1>
-          <div className="inv-header-row">
-            <div className="inv-column">Categories</div>
-            <div className="inv-column">Total Products</div>
-            <div className="inv-column">Top Selling</div>
-            <div className="inv-column">Low Stocks</div>
-          </div>
+      <div className="overall-inventory-container container-rounded">
+    <h1 className="overall-inventory-header">Overall Inventory</h1>
+    <div className="overall-header-row">
+        <div className="overall-column">
+            Categories
+            <span className="column-text">14</span>
+            <span className="column-subtext">Last 7 Days</span>
         </div>
-        <div className="h-48 rounded-lg bg-white-950"></div>
-      </div>
+        <div className="overall-column">
+            Total Products
+            <span className="column-text">342</span>
+            <span className="column-subtext">Last 7 Days</span>
+        </div>
+        <div className="overall-column">
+            Top Selling
+            <span className="column-text">5</span>
+            <span className="column-subtext">Last 7 Days</span>
+        </div>
+        <div className="overall-column">
+            Low Stocks
+            <span className="column-text">12</span>
+            <span className="column-subtext">Ordered</span>
+        </div>
+    </div>
+</div>
     </div>
   );
 };
