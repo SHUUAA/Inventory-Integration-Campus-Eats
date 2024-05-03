8734bd0b48bf186090c497925aac5e4e418127d6
yunyun <ianmathewaviso@gmail.com>
2024-05-03 15:43:03 +0800

    Fix dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -6,128 +6,128 @@ import tata from '/assets/temporary/tata.png'; // Changed the import name here
 const Dashboard = () => {
 
   return (
-      <div className='main-content'>
-        
+    <div className='main-content'>
 
-        {/* 1st CardBox */}
-        <div className='cardBox'>
+
+      {/* 1st CardBox */}
+      <div className='cardBox'>
         {/* Sales */}
-          <div className='card'>
-            <div>
-                <div className='salesNumber'>₱832</div>
-                <div className='name'>Sales</div>
-            </div>
-              <img src="/assets/cash-outline.svg" alt="cash" className='icon1'/>
+        <div className='card'>
+          <div>
+            <div className='salesNumber'>₱832</div>
+            <div className='name'>Sales</div>
           </div>
+          <img src="/assets/cash-outline.svg" alt="cash" className='icon1' />
+        </div>
 
-          {/* Quantity in Hand */}
-          <div className='card'>
-            <div>
-                <div className='salesNumber'>310</div>
-                <div className='name'>Quantity in Hand</div>
-            </div>
-              <img src="/assets/cube-outline.svg" alt="cash" className='icon1'/>
-            
+        {/* Quantity in Hand */}
+        <div className='card'>
+          <div>
+            <div className='salesNumber'>310</div>
+            <div className='name'>Quantity in Hand</div>
           </div>
-
-          {/* Purchase */}
-          <div className='card'>
-            <div>
-                <div className='salesNumber'>82</div>
-                <div className='name'>Purchase</div>
-            </div>
-            
-            <img src="/assets/bag-check-outline.svg" alt="cash" className='icon1'/>
-         
-          </div>
-
-          {/* Number of Suppliers */}
-          <div className='card'>
-            <div>
-                <div className='salesNumber'>31</div>
-                <div className='name'>Number of Suppliers</div>
-            </div>
-            
-            <img src="/assets/people-outline.svg" alt="cash" className='icon1'/>
-            
-          </div>
+          <img src="/assets/cube-outline.svg" alt="cash" className='icon1' />
 
         </div>
 
-         {/* Top Selling Stock */}
-        <div className='details'>
-            <div className='topSelling'>
-              <div className='cardHeader'>
-                <h2>Top Selling Stock</h2>
-                <a href='#' className='btn'>See All</a> {/* Changed class to className */}
-              </div>
+        {/* Purchase */}
+        <div className='card'>
+          <div>
+            <div className='salesNumber'>82</div>
+            <div className='name'>Purchase</div>
+          </div>
 
-              <table>
-                  <thead>
-                      <tr>
-                        <td>Name</td>
-                        <td>Sold Quantity</td>
-                        <td>Remaining Quantity</td>
-                        <td>Price</td>
-                      </tr>
-                  </thead>
+          <img src="/assets/bag-check-outline.svg" alt="cash" className='icon1' />
 
-                  <tbody>
-                    <tr>
-                        <td>Surf Excel</td>
-                        <td>30</td>
-                        <td>12</td>
-                        <td>₱100</td>
-                    </tr>
+        </div>
 
-                    <tr>
-                        <td>Rin</td>
-                        <td>21</td>
-                        <td>15</td>
-                        <td>₱207</td>
-                    </tr>
+        {/* Number of Suppliers */}
+        <div className='card'>
+          <div>
+            <div className='salesNumber'>31</div>
+            <div className='name'>Number of Suppliers</div>
+          </div>
 
-                    <tr>
-                        <td>Parle G</td>
-                        <td>19</td>
-                        <td>17</td>
-                        <td>₱105</td>
-                    </tr>
-                  </tbody>
-              </table>
-            </div>
+          <img src="/assets/people-outline.svg" alt="cash" className='icon1' />
 
-            <div className='lowStock'>
-                <div className='cardHeader'>
-                  <h2>Low Quantity Stock</h2>
-                  <a href='#' className='btn'>See All</a> {/* Changed class to className */}
-                </div>
+        </div>
 
-                <table>
-                    <tr>
-                        <td width="60px">
-                          <div className='imgBx'><img src={lays} alt="lays" className="tata" /></div>
-                        </td>
-                        <td>
-                          <h4>Lays <br></br> <span>Remaining Quantity: 12</span> </h4>
-                        </td>
-                        <td><span className='low'>Low</span></td>
-                    </tr>
+      </div>
 
-                    <tr>
-                    <td width="60px">
-                          <div className='imgBx'><img src={tata} alt="Tata" className="tata" /></div>
-                        </td>
-                        <td>
-                          <h4>Tata Salt <br></br> <span>Remaining Quantity: 12</span></h4>
-                        </td>
-                        <td><span className='low'>Low</span></td>
-                    </tr>
-              </table>
-            </div>
+      {/* Top Selling Stock */}
+      <div className='details'>
+        <div className='topSelling'>
+          <div className='cardHeader'>
+            <h2>Top Selling Stock</h2>
+            <a href='#' className='btn'>See All</a> {/* Changed class to className */}
+          </div>
+
+          <table>
+            <thead>
+              <tr>
+                <td>Name</td>
+                <td>Sold Quantity</td>
+                <td>Remaining Quantity</td>
+                <td>Price</td>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td>Surf Excel</td>
+                <td>30</td>
+                <td>12</td>
+                <td>₱100</td>
+              </tr>
+
+              <tr>
+                <td>Rin</td>
+                <td>21</td>
+                <td>15</td>
+                <td>₱207</td>
+              </tr>
+
+              <tr>
+                <td>Parle G</td>
+                <td>19</td>
+                <td>17</td>
+                <td>₱105</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div className='lowStock'>
+          <div className='cardHeader'>
+            <h2>Low Quantity Stock</h2>
+            <a href='#' className='btn'>See All</a> {/* Changed class to className */}
+          </div>
+
+          <table>
+            <tr>
+              <td width="60px">
+                <div className='imgBx'><img src={lays} alt="lays" className="tata" /></div>
+              </td>
+              <td>
+                <h4>Lays <br></br> <span>Remaining Quantity: 12</span> </h4>
+              </td>
+              <td><span className='low'>Low</span></td>
+            </tr>
+
+            <tr>
+              <td width="60px">
+                <div className='imgBx'><img src={tata} alt="Tata" className="tata" /></div>
+              </td>
+              <td>
+                <h4>Tata Salt <br></br> <span>Remaining Quantity: 12</span></h4>
+              </td>
+              <td><span className='low'>Low</span></td>
+            </tr>
+          </table>
         </div>
       </div>
-      
+    </div>
+
   );
 }
 
