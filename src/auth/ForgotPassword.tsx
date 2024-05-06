68be14fcdb228047e6d899f8f48bca7878bdbf4f
momoyaan <ianmathewaviso@gmail.com>
2024-05-06 22:13:48 +0800

    Update forgot password page. (Please npm install again)

--- src/auth/ForgotPassword.tsx
+++ src/auth/ForgotPassword.tsx
@@ -1,5 +1,4 @@
-import { sendPasswordResetEmail } from 'firebase/auth';
-import { authentication } from '../config/firebase'; 
+
 
 const actionCodeSettings = {
   url: 'http://localhost:5173/reset-password?oobCode=CODE', // Replace with your app's password reset page
@@ -9,12 +8,10 @@ const actionCodeSettings = {
 
 const forgotpassword = async (email: string) => {
   try {
-    const response = await sendPasswordResetEmail(authentication, email);
-    console.log('Password reset email sent!');
+    await sendPasswordResetEmail(authentication, email);
     // Notify user of success (e.g., display a message)
-    return response;
   } catch (error) {
-    console.error('Error sending reset email:', error);
+    //console.error('Error sending reset email:', error);
     // Notify user of error (e.g., display an error message)
   }
 };
--- src/pages/ForgotPasswordPage.tsx
+++ src/pages/ForgotPasswordPage.tsx
@@ -1,62 +1,95 @@
-import React from "react";
 import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 import "../css/ForgotPasswordPage.css";
-import forgotpassword from "../auth/ForgotPassword";
+import { sendPasswordResetEmail } from 'firebase/auth';
+import { authentication } from '../config/firebase'; 
+
+const actionCodeSettings = {
+  url: 'http://localhost:5173/reset-password?oobCode=CODE', // Replace with your app's password reset page
+  handleCodeInApp: true  // Handle the link in your app
+};
 
 const ForgotPassword = () => {
-    const [email, setEmail] = useState('');
-    const [emailFocus, setEmailFocus] = useState(false);
-    const [validEmail, setValidEmail] = useState(true);
+  const [email, setEmail] = useState("");
+  const [emailFocus, setEmailFocus] = useState(false);
+  const [validEmail, setValidEmail] = useState(true);
 
-
-    const handleGetCodeClick = (e) => {
-        e.preventDefault();
-
-        forgotpassword(email);
-        
+  const handleGetCodeClick = async (e) => {
+    try {
+    e.preventDefault()
+    await sendPasswordResetEmail(authentication, email);
+    toast.success("If this user exists, we have sent you a password reset email.");
+    } catch (error) {
+        toast.error("Please enter a valid email.");
     }
+  };
 
+  return (
+    <main className="fp-main">
+      <div>
+        <Toaster
+          position="top-center"
+          reverseOrder={false}
+          gutter={8}
+          containerClassName=""
+          containerStyle={{}}
+          toastOptions={{
+            // Define default options
+            className: "",
+            duration: 5000,
+            style: {
+              background: "#DFD5C5",
+            },
 
-
-    return (
-        <main className="fp-main">
-            <div className="fp-box" style={{ height: '320px' }}>
-                <div className="fp-inner-box">
-                    <div className="fp-forms-wrap">
-                        <form className="fp-form">
-                            <div className="fp-header">
-                               <h1>Forgot Password</h1>
-                                <span className="small-text">Enter your email and we'll send you a reset password link!</span>
-                            </div>
-                            <div className="fp-actual-form">
-                                <div className="fp-input-wrap">
-                                    <input
-                                        type="text"
-                                        id="email"
-                                        required
-                                        className={`fp-input-field ${emailFocus || email ? 'active' : ''}`}
-                                        onChange={(e) => setEmail(e.target.value)}
-                                        aria-invalid={validEmail ? "false" : "true"}
-                                        aria-describedby="uidnote"
-                                        onFocus={()=> setEmailFocus(true)}
-                                        onBlur={()=> setEmailFocus(false)}
-                                        
-                                    />
-                                    <label>Email</label>
-                                </div>
-                                
-                                <button onClick={ handleGetCodeClick } className="fp-btn">
-                                    Send Email
-                                </button>
-                            </div>
-                            
-                        
-                        </form>
-                    </div>
+            // Default options for specific types
+            success: {
+              duration: 5000,
+              theme: {
+                primary: "green",
+                secondary: "black",
+              },
+            },
+          }}
+        />
+      </div>
+      <div className="fp-box" style={{ height: "320px" }}>
+        <div className="fp-inner-box">
+          <div className="fp-forms-wrap">
+            <form className="fp-form">
+              <div className="fp-header">
+                <h1>Forgot Password</h1>
+                <span className="small-text">
+                  Enter your email and we'll send you a reset password link!
+                </span>
+              </div>
+              <div className="fp-actual-form">
+                <div className="fp-input-wrap">
+                  <input
+                    type="text"
+                    id="email"
+                    required
+                    className={`fp-input-field ${
+                      emailFocus || email ? "active" : ""
+                    }`}
+                    onChange={(e) => setEmail(e.target.value)}
+                    aria-invalid={validEmail ? "false" : "true"}
+                    aria-describedby="uidnote"
+                    onFocus={() => setEmailFocus(true)}
+                    onBlur={() => setEmailFocus(false)}
+                  />
+                  <label>Email</label>
                 </div>
-            </div>
-        </main>
-    );
-}
+
+                <button onClick={handleGetCodeClick} className="fp-btn">
+                  Send Email
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
 
 export default ForgotPassword;
