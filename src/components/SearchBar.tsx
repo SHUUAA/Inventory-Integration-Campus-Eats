b0d603774fc02072d3fb9da32310a3c8c6504838
momoyaan <ianmathewaviso@gmail.com>
2024-05-17 10:19:03 +0800

    update SearchBar.tsx

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -38,7 +38,7 @@ const SearchBar: React.FC = () => {
             Campus Eats
           </span>
         </a>
-        <form>
+        {/* <form>
           <label
             for="default-search"
             class="mb-2 text-sm font-medium text-black sr-only"
@@ -70,11 +70,11 @@ const SearchBar: React.FC = () => {
               required
             />
           </div>
-        </form>
+        </form> */}
         <div className="flex"></div>
         <div className="flex w-full md:w-auto md:order-1">
           <div className="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
-            <div className="inline-flex h-[45px] w-[45px] select-none items-center justify-center overflow-hidden rounded-full align-middle">
+            {/* <div className="inline-flex h-[45px] w-[45px] select-none items-center justify-center overflow-hidden rounded-full align-middle">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -89,7 +89,7 @@ const SearchBar: React.FC = () => {
                   d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
                 />
               </svg>
-            </div>
+            </div> */}
             <Avatar.Root className="inline-flex h-[45px] w-[45px] select-none items-center justify-center overflow-hidden rounded-full align-middle">
               <Link to={`/${userData.type}/profile`}>
                 <Avatar.AvatarImage
